48028ffb7bea089e98946cc2a8f7dedf59988a76
zaizai <327120052@qq.com>
2022-09-09 23:31:27 +0800

    20220909 LeetCode 0236 二叉树的最近公共祖先

---
+++ LeetCode 0236 二叉树的最近公共祖先.cpp
@@ -0,0 +1,34 @@
+// 分类：DFS
+// Created by 旺崽 on 2021/11/16.
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == nullptr || root == p || root == q) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left == nullptr) return right;
+        if (right == nullptr) return left;
+        return root;
+    }
+};
+
+int main() {
+    return 0;
+}
