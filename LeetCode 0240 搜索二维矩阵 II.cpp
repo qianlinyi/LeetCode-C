ec693428f7ed7e1b7eb79053c2cbb4a26aa77c53
zaizai <327120052@qq.com>
2022-09-10 22:43:30 +0800

    20220910 LeetCode 0240 搜索二维矩阵 II

---
+++ LeetCode 0240 搜索二维矩阵 II.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 旺崽 on 2021/11/24.
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) return true;
+            if (matrix[x][y] > target) y--;
+            else x++;
+        }
+        return false;
+    }
+};
+
+int main() {
+    return 0;
+}
