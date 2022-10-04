5f084f98c75cb4c2e7e8a8c73d660d5e64c93cf7
zaizai <327120052@qq.com>
2022-10-04 10:51:08 +0800

    20221004 LeetCode 剑指 Offer 16 数值的整数次方

---
+++ LeetCode 剑指 Offer 16 数值的整数次方.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 旺崽 on 2021/12/5.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1, flag = 0, a = x;
+        long long e = n;
+        if (e < 0) e = -e, flag = 1;
+        while (e) {
+            if (e & 1) ans = ans * a;
+            a = a * a;
+            e >>= 1;
+        }
+        if (flag) return 1 / ans;
+        return ans;
+    }
+};
+
+int main() {
+
+    return 0;
+}
