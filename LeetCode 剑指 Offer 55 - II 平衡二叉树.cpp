2043a037f9668de038ff583983491b9b379c38bc
zaizai <327120052@qq.com>
2022-11-11 12:05:19 +0800

    20221111 LeetCode 剑指 Offer 55 - II 平衡二叉树 DFS

---
+++ LeetCode 剑指 Offer 55 - II 平衡二叉树.cpp
@@ -0,0 +1,43 @@
+//
+// Created by 旺崽 on 2021/12/3.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) >= 0;
+    }
+};
+
+int main() {
+    return 0;
+}
