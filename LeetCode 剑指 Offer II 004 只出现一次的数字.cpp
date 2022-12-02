443c2968b4e1a48827aa3c946fdcea5720af2b42
zaizai <327120052@qq.com>
2022-12-02 12:10:12 +0800

    20221202 LeetCode 剑指 Offer II 004 只出现一次的数字 位运算

---
+++ LeetCode 剑指 Offer II 004 只出现一次的数字.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 崽崽 on 2022/12/2.
+//
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int> &nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int total = 0;
+            for (int num: nums) {
+                total += ((num >> i) & 1);
+            }
+            if (total % 3) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+};
